3534183ecd29244d87b4e1a60d82b751fb6cafd9
Nimcode-05 <2005nimisha.s@gmail.com>
2026-04-09 19:53:29 +0530

    Create Perfectnum.py

---
+++ Day15/Perfectnum.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num<=1:
+            return False
+        sum=1
+        for i in range(2,int(math.sqrt(num))+1):
+            if num%i==0:
+                sum+=i
+                if i!=num//i:
+                    sum+=num//i
+        if sum==num:
+            return True  
+        else:
+            return False                  
